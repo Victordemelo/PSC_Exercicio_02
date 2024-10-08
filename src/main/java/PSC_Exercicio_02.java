c0b9a8740d9565d3842cdb3c8ebac5fac5879bc9
Victor de Melo <victorgameplays535@gmail.com>
2024-10-08 10:38:56 -0300

    update: removendo os comentários gerados pela IDE

--- src/main/java/PSC_Exercicio_02.java
+++ src/main/java/PSC_Exercicio_02.java
@@ -1,37 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
+//@author victo
 
+import java.util.Scanner;
 
-
-/**
- *
- * @author victo
- */import java.util.Scanner;
 public class PSC_Exercicio_02 {
 
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        
+
         double salario, prestacao, emprestimo;
-        
+
         System.out.println("Digite o valor do Salário: ");
         salario = leitor.nextDouble();
         System.out.println("Digite o valor da Prestação: ");
         prestacao = leitor.nextDouble();
-        
-        emprestimo = salario*0.3;
-        
-        if (prestacao < emprestimo){
+
+        emprestimo = salario * 0.3;
+
+        if (prestacao < emprestimo) {
             System.out.println("Empréstimo Concedido");
         } else {
             System.out.println("Empréstimo NEGADO");
         }
-        
-        
-        
-    
-        
+
     }
 }
